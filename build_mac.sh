30ee25264d2c0d461a7164583f02e6de82ec94ef
Jinwoo Park <pmnxis@gmail.com>
2025-10-19 20:32:34 +0900

    minor change

--- build_mac.sh
+++ build_mac.sh
@@ -18,14 +18,16 @@ rustup target add aarch64-apple-darwin
 cargo bundle --release
 
 echo otool for aarch64:
-otool -L target/aarch64-apple-darwin/release/chama-optics
-# lipo -create -output target/release/bundle/osx/chama-optics.app/Contents/MacOS/chama-optics target/x86_64-apple-darwin/release/chama-optics target/aarch64-apple-darwin/release/chama-optics 
-file target/release/bundle/osx/chama-optics.app/Contents/MacOS/chama-optics
+# otool -L target/aarch64-apple-darwin/release/chama-optics
+echo "# Linked shared library"
+otool -L target/release/chama-optics
+# lipo -create -output target/release/bundle/osx/Chama\ Optics.app/Contents/MacOS/chama-optics target/x86_64-apple-darwin/release/chama-optics target/aarch64-apple-darwin/release/chama-optics 
+file target/release/bundle/osx/Chama\ Optics.app/Contents/MacOS/chama-optics
 # echo otool for universal binary:
-# otool -L target/release/bundle/osx/chama-optics.app/Contents/MacOS/chama-optics
+# otool -L target/release/bundle/osx/Chama\ Optics.app/Contents/MacOS/chama-optics
 
-mkdir target/release/bundle/osx/chama-optics.app/Contents/Frameworks/
-chmod +rw target/release/bundle/osx/chama-optics.app/Contents/Frameworks/*
+mkdir target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/
+chmod +rw target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/*
 
 libs=( /opt/homebrew/opt/x265/lib/libx265.215.dylib /opt/homebrew/opt/libheif/lib/libheif.1.dylib /opt/homebrew/opt/x265/lib/libx265.209.dylib /opt/homebrew/opt/libde265/lib/libde265.0.dylib /opt/homebrew/opt/aom/lib/libaom.3.dylib /opt/homebrew/opt/webp/lib/libsharpyuv.0.dylib /opt/homebrew/opt/libvmaf/lib/libvmaf.3.dylib )
 
@@ -33,34 +35,38 @@ for lib in ${libs[@]}
 do
     echo COPY $lib
     # deploy lib
-    cp $lib target/release/bundle/osx/chama-optics.app/Contents/Frameworks/
-    install_name_tool -add_rpath "@executable_path/../Frameworks/$(basename $lib)" target/release/bundle/osx/chama-optics.app/Contents/MacOS/chama-optics
-    # install_name_tool -change $lib "@executable_path/../Frameworks/$(basename $lib)" target/release/bundle/osx/chama-optics.app/Contents/MacOS/chama-optics
+    cp $lib target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/
+    install_name_tool -add_rpath "@executable_path/../Frameworks/$(basename $lib)" target/release/bundle/osx/Chama\ Optics.app/Contents/MacOS/chama-optics
+    # install_name_tool -change $lib "@executable_path/../Frameworks/$(basename $lib)" target/release/bundle/osx/Chama\ Optics.app/Contents/MacOS/chama-optics
 
 done
 
-install_name_tool -change /opt/homebrew/opt/x265/lib/libx265.209.dylib "@executable_path/../Frameworks/libx265.209.dylib"       target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libheif.1.dylib
-install_name_tool -change /opt/homebrew/opt/libde265/lib/libde265.0.dylib "@executable_path/../Frameworks/libde265.0.dylib"     target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libheif.1.dylib
-install_name_tool -change /opt/homebrew/opt/libheif/lib/libheif.1.dylib "@executable_path/../Frameworks/libheif.1.dylib"        target/release/bundle/osx/chama-optics.app/Contents/MacOS/chama-optics
-install_name_tool -change /opt/homebrew/opt/aom/lib/libaom.3.dylib "@executable_path/../Frameworks/libaom.3.dylib"              target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libheif.1.dylib
-install_name_tool -change /opt/homebrew/opt/webp/lib/libsharpyuv.0.dylib "@executable_path/../Frameworks/libsharpyuv.0.dylib"   target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libheif.1.dylib
-install_name_tool -change /opt/homebrew/opt/libvmaf/lib/libvmaf.3.dylib "@executable_path/../Frameworks/libvmaf.3.dylib"        target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libaom.3.dylib
-install_name_tool -change /opt/homebrew/lib/libx265.215.dylib "@executable_path/../Frameworks/libx265.215.dylib"                target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libheif.1.dylib
-install_name_tool -change /opt/homebrew/opt/x265/lib/libx265.215.dylib "@executable_path/../Frameworks/libx265.215.dylib"       target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libheif.1.dylib
+install_name_tool -change /opt/homebrew/opt/x265/lib/libx265.209.dylib "@executable_path/../Frameworks/libx265.209.dylib"       target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libheif.1.dylib
+install_name_tool -change /opt/homebrew/opt/libde265/lib/libde265.0.dylib "@executable_path/../Frameworks/libde265.0.dylib"     target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libheif.1.dylib
+install_name_tool -change /opt/homebrew/opt/libheif/lib/libheif.1.dylib "@executable_path/../Frameworks/libheif.1.dylib"        target/release/bundle/osx/Chama\ Optics.app/Contents/MacOS/chama-optics
+install_name_tool -change /opt/homebrew/opt/aom/lib/libaom.3.dylib "@executable_path/../Frameworks/libaom.3.dylib"              target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libheif.1.dylib
+install_name_tool -change /opt/homebrew/opt/webp/lib/libsharpyuv.0.dylib "@executable_path/../Frameworks/libsharpyuv.0.dylib"   target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libheif.1.dylib
+install_name_tool -change /opt/homebrew/opt/libvmaf/lib/libvmaf.3.dylib "@executable_path/../Frameworks/libvmaf.3.dylib"        target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libaom.3.dylib
+install_name_tool -change /opt/homebrew/lib/libx265.215.dylib "@executable_path/../Frameworks/libx265.215.dylib"                target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libheif.1.dylib
+install_name_tool -change /opt/homebrew/opt/x265/lib/libx265.215.dylib "@executable_path/../Frameworks/libx265.215.dylib"       target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libheif.1.dylib
 
 for lib in ${libs[@]}
 do
     # sign lib
     echo SIGN $lib
-    codesign -s "-" -fv target/release/bundle/osx/chama-optics.app/Contents/Frameworks/$(basename $lib)
+    codesign -s "-" -fv target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/$(basename $lib)
 done
-
-cp Info.plist target/release/bundle/osx/chama-optics.app/Contents/Info.plist
 
 # echo try this to test the build:
 # echo brew uninstall libheif ffmpeg
 # brew uninstall libheif ffmpeg
-echo you can test target/release/bundle/osx/chama-optics.app now
 
-otool -L target/release/bundle/osx/chama-optics.app/Contents/MacOS/chama-optics
-otool -L target/release/bundle/osx/chama-optics.app/Contents/Frameworks/libheif.1.dylib
+echo "# Check inner binary again"
+otool -L target/release/bundle/osx/Chama\ Optics.app/Contents/MacOS/chama-optics
+otool -L target/release/bundle/osx/Chama\ Optics.app/Contents/Frameworks/libheif.1.dylib
+
+echo "###########################################################"
+echo ""
+echo you can test target/release/bundle/osx/Chama\ Optics.app now
+echo ""
+echo "###########################################################"
